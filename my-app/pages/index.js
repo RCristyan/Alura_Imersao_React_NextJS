a92701116981e8311d11c2ed0fd5c3181d5f1c27
RCristyan <rcristyan9@gmail.com>
2021-01-26 18:16:13 -0300

    Ajustes sugeridos pelo eslint.

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,46 +1,52 @@
-import styled from 'styled-components'
-import db from '../db.json'
-import Widget from '../src/components/Widget'
-import QuizBackground from '../src/components/QuizBackground'
-import Footer from '../src/components/Footer'
-import GithubCorner from '../src/components/GithubCorner'
-import QuizContainer from '../src/components/QuizContainer'
+import Head from 'next/head';
+import React from 'react';
+
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import GithubCorner from '../src/components/GithubCorner';
+import QuizContainer from '../src/components/QuizContainer';
 
 export default function Home() {
-    return (
-        <QuizBackground backgroundImage={db.bg}>
-            <QuizContainer>
-                <Widget>
-                    <Widget.Header>
-                        <h1>The Legend of Zelda</h1>
-                    </Widget.Header>
-                    <Widget.Content>
-                        <p>
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                            Voluptatibus, quod suscipit consequuntur nam nesciunt sequi quidem,
-                            incidunt sunt sint adipisci culpa explicabo,
-                            rem sed reiciendis quisquam inventore et enim modi.
-                        </p>
-                    </Widget.Content>
-                </Widget>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>AluraQuiz - Modelo Base</title>
+      </Head>
 
-                <Widget>
-                    <Widget.Header>
-                        <h1>Quizes da galera</h1>
-                    </Widget.Header>
-                    <Widget.Content>
-                        <p>
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                            Voluptatibus, quod suscipit consequuntur nam nesciunt sequi quidem,
-                            incidunt sunt sint adipisci culpa explicabo,
-                            rem sed reiciendis quisquam inventore et enim modi.
-                        </p>
-                    </Widget.Content>
-                </Widget>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+            <h1>The Legend of Zelda</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Voluptatibus, quod suscipit consequuntur nam nesciunt sequi
+              quidem, incidunt sunt sint adipisci culpa explicabo, rem sed
+              reiciendis quisquam inventore et enim modi.
+            </p>
+          </Widget.Content>
+        </Widget>
 
-                <Footer />
-            </QuizContainer>
-            <GithubCorner projectUrl="https://github.com/RCristyan"/>
-        </QuizBackground>
-    )
+        <Widget>
+          <Widget.Header>
+            <h1>Quizes da galera</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Voluptatibus, quod suscipit consequuntur nam nesciunt sequi
+              quidem, incidunt sunt sint adipisci culpa explicabo, rem sed
+              reiciendis quisquam inventore et enim modi.
+            </p>
+          </Widget.Content>
+        </Widget>
+
+        <Footer />
+      </QuizContainer>
+      <GithubCorner projectUrl="https://github.com/RCristyan" />
+    </QuizBackground>
+  );
 }
