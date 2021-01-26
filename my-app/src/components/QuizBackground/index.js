1d39e594a29f1afece8b9f28a083fb259f953320
RCristyan <rcristyan9@gmail.com>
2021-01-26 14:03:57 -0300

    Alterado tratamento de url de background.

--- my-app/src/components/QuizBackground/index.js
+++ my-app/src/components/QuizBackground/index.js
@@ -1,11 +1,10 @@
 import styled from 'styled-components';
-import db from '../../../db.json'
 
 const QuizBackground = styled.div`
     width: 100%;
     background-size: cover;
     background-position: center;
-    background-image: url(${db.bg});
+    background-image: url(${({ backgroundImage }) => backgroundImage});
     background-color: ${({ theme }) => theme.colors.mainBg};
     flex: 1;
     
