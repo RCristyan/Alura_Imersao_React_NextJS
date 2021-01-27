68f77b5673ded8717b1f1aede0330e17625a56a7
RCristyan <rcristyan9@gmail.com>
2021-01-27 20:45:40 -0300

    Implementada versao inicial da pagina de quiz.

--- my-app/pages/quiz.js
+++ my-app/pages/quiz.js
@@ -1,10 +1,47 @@
 import React from 'react';
 
+import db from '../db.json';
+import Button from '../src/components/Button';
+import QuizBackground from '../src/components/QuizBackground';
+import QuizContainer from '../src/components/QuizContainer';
+import QuizLogo from '../src/components/QuizLogo';
+import Widget from '../src/components/Widget';
+
 function QuizPage() {
   return (
-    <div>
-      Hello world
-    </div>
+    <QuizBackground>
+      <QuizContainer>
+        <QuizLogo />
+        <Widget>
+          <Widget.Header>
+            <h3>
+              Pergunta 1 de
+              {` ${db.questions.length}`}
+            </h3>
+          </Widget.Header>
+
+          <img
+            src="https://placehold.it/400x400"
+            alt="Descrição"
+            style={{
+              width: '100%',
+              height: '150px',
+              objectFit: 'cover',
+            }}
+          />
+
+          <Widget.Content>
+            <h2>Título</h2>
+            <p>
+              Descrição
+            </p>
+            <Button>
+              Confirmar
+            </Button>
+          </Widget.Content>
+        </Widget>
+      </QuizContainer>
+    </QuizBackground>
   );
 }
 
