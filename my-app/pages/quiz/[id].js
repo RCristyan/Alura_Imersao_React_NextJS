e20e5a9610c0c2b98609fbe2dbb77379cb8adcc1
RCristyan <rcristyan9@gmail.com>
2021-01-29 20:35:14 -0300

    Adicionada função getServerSideProps()

--- my-app/pages/quiz/[id].js
+++ my-app/pages/quiz/[id].js
@@ -8,4 +8,24 @@ function QuizDaGaleraPage() {
   );
 }
 
+export async function getServerSideProps(context) {
+  fetch('')
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error('Falha em obter os dados');
+    })
+    .then((respostaConvertedToObject) => {
+      console.log(respostaConvertedToObject);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+
+  return {
+    props: {}, // will be passed to the page components as props
+  };
+}
+
 export default QuizDaGaleraPage;
